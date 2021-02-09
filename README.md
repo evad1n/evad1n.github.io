11201841eadcf561a3819ffd855e4483db3e05c7
Will Dickinson <willdickinson98@gmail.com>
2021-02-09 00:10:48 -0700

    added particles and maybe typer idk git yolo

--- README.md
+++ README.md
@@ -2,25 +2,4 @@
 
 [Link](https://evad1n.github.io/)
 
-# TODO: 
-- animations
-- template this repo cuz yee
-
-## Build Setup
-
-```bash
-# install dependencies
-$ yarn install
-
-# serve with hot reload at localhost:3000
-$ yarn dev
-
-# build for production and launch server
-$ yarn build
-$ yarn start
-
-# generate static project
-$ yarn generate
-```
-
-For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+Made using [Nuxt.js](https://nuxtjs.org/) and [Vuetify](https://vuetifyjs.com/en/).
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,19 @@
 <template>
     <v-app>
         <Nuxt />
+        <client-only>
+            <particles />
+        </client-only>
     </v-app>
 </template>
 
 <script>
+import Particles from '~/components/Particles';
+
 export default {
+    components: {
+        Particles
+    },
     data() {
         return {};
     }
@@ -22,5 +30,17 @@ html {
 
 .v-application {
     background-color: rgba($color: #000000, $alpha: 0) !important;
+}
+
+div[id^='particles-instance-'] {
+    height: 100vh !important;
+    width: 100vw !important;
+    position: fixed !important;
+    top: 0 !important;
+    margin: 0 !important;
+    padding: 0 !important;
+    overflow: hidden !important;
+    background: rgba($color: #05114e, $alpha: 0.4);
+    z-index: 2 !important;
 }
 </style>
